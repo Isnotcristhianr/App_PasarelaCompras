0607b218565fc2e2b34d3aff1a957a0e3a6ec3bf
Cristhian Recalde <recaldecd@gmail.com>
2023-01-31 18:37:40 -0600

    pantalla splash

--- App_PasarelaCompras.Android/MainActivity.cs
+++ App_PasarelaCompras.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 //dialogs
 namespace App_PasarelaCompras.Droid
 {
-    [Activity(Label = "App_PasarelaCompras", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "App_PasarelaCompras", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ App_PasarelaCompras.Android/splashScreen.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace App_PasarelaCompras.Droid
+{
+   // [Activity(Label = "splashScreen")]
+    [Activity(Label = "App_PasarelaCompras", Icon = "@mipmap/icon", Theme = "@style/temaApp", MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize)]
+
+    public class splashScreen : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            //enviar suscripcion de anterior configuracion de inicio
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+            // Create your application here
+
+
+        }
+    }
+}
